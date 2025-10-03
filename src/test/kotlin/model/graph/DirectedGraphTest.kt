80fd0d80380cc995f558ad1df7427778547ae0a0
Sterkachov <anymeshnik2.1@gmail.com>
2025-10-03 10:18:27 +0300

    feat(model.graph): покрыл unit тестами 4 графа

---
+++ src/test/kotlin/model/graph/DirectedGraphTest.kt
@@ -0,0 +1,98 @@
+package model.graph
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import kotlin.test.assertSame
+
+class DirectedGraphTest {
+
+    @Test
+    fun testCreateVertex() {
+        val graph = DirectedGraph<String, String>()
+        val v1 = graph.addVertex("A")
+        val v2 = graph.addVertex("b")
+
+        assertEquals(2, graph.vertices.size)
+        assertTrue(graph.vertices.contains(v1))
+        assertTrue(graph.vertices.contains(v2))
+    }
+
+    @Test
+    fun testVertexNotDuplicates() {
+        val graph = DirectedGraph<String, String>()
+
+        val v1 = graph.addVertex("A")
+        val v2 = graph.addVertex("A")
+
+        assertEquals(1, graph.vertices.size)
+        assertSame(v1, v2)
+    }
+
+    @Test
+    fun testCreateEdge() {
+        val graph = DirectedGraph<String, String>()
+
+        val v1 = graph.addVertex("A")
+        val v2 = graph.addVertex("B")
+
+        val e1 = graph.addEdge("A", "B", "edge1")
+
+        assertEquals(1, graph.edges.size)
+        assertTrue(e1.incident(v1))
+        assertTrue(e1.incident(v2))
+        assertEquals(Pair(v1, v2), e1.vertices)
+    }
+
+    @Test
+    fun testEdgeNotDuplicates() {
+        val graph = DirectedGraph<String, String>()
+
+        val e1 = graph.addEdge("A", "B", "edge1")
+        val e2 = graph.addEdge("A", "B", "edge1")
+
+        assertEquals(1, graph.edges.size)
+        assertSame(e1, e2)
+    }
+
+    @Test
+    fun testIncidentDetectOnlyCorrectVertices() {
+        val graph = DirectedGraph<String, String>()
+        val v1 = graph.addVertex("X")
+        val v2 = graph.addVertex("Y")
+        val v3 = graph.addVertex("")
+
+        val edge = graph.addEdge("X", "Y", "e")
+
+        assertTrue(edge.incident(v1))
+        assertTrue(edge.incident(v2))
+        assertFalse(edge.incident(v3))
+    }
+
+    @Test
+    fun testRemoveEdgeDeleteEdge() {
+        val graph = DirectedGraph<String, String>()
+        graph.addVertex("A")
+        graph.addVertex("B")
+
+        graph.addEdge("A", "B", "e1")
+        val removed = graph.removeEdge("e1")
+
+        assertTrue(removed)
+        assertTrue(graph.edges.isEmpty())
+    }
+
+    @Test
+    fun testRemoveVertexDeleteVertexAndIncidentEdges() {
+        val graph = DirectedGraph<String, String>()
+
+        graph.addEdge("A", "B", "e1")
+        graph.addEdge("B", "C", "e2")
+        val removed = graph.removeVertex("B")
+
+        assertTrue(removed)
+        assertEquals(2, graph.vertices.size)
+        assertTrue(graph.edges.isEmpty())
+    }
+}
