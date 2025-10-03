5c1248727101132d74dc81a66ca8e435fc879028
Sterkachov <anymeshnik2.1@gmail.com>
2025-10-03 11:07:46 +0300

    feat(model.graph): добавил алгоритм построения минимального остовного дерева и покрыл его юнит тестами

---
+++ src/main/kotlin/model/graph/algorithms/MinimumSpanningTree.kt
@@ -0,0 +1,53 @@
+package model.graph.algorithms
+
+import model.graph.WeightedUndirectedGraph
+import model.graph.base.Vertex
+import model.graph.weighted.WeightedEdge
+
+internal class MinimumSpanningTree<E, V>(val graph: WeightedUndirectedGraph<E, V>) {
+    fun kruskalAlgo() : List<WeightedEdge<E, V>> {
+        val mst = mutableListOf<WeightedEdge<E, V>>()
+
+        val parent = mutableMapOf<Vertex<V>, Vertex<V>>()
+        val rank = mutableMapOf<Vertex<V>, Int>()
+
+        for (v in graph.vertices) {
+            parent[v] = v
+            rank[v] = 0
+        }
+
+        val sortedEdges = graph.edges.sortedBy { it.weight }
+
+        fun find(v: Vertex<V>): Vertex<V> {
+            if (parent[v] != v) {
+                parent[v] = find(parent[v]!!)
+            }
+            return parent[v]!!
+        }
+
+        fun union(u: Vertex<V>, v: Vertex<V>): Boolean {
+            val rootU = find(u)
+            val rootV = find(v)
+            if (rootU == rootV) return false
+            if (rank[rootU]!! < rank[rootV]!!) {
+                parent[rootU] = rootV
+            } else if (rank[rootU]!! > rank[rootV]!!) {
+                parent[rootV] = rootU
+            } else {
+                parent[rootV] = rootU
+                rank[rootU] = rank[rootU]!! + 1
+            }
+            return true
+        }
+
+        for (edge in sortedEdges) {
+            val u = edge.vertices.first
+            val v = edge.vertices.second
+            if (union(u, v)) {
+                mst += edge
+            }
+        }
+
+        return mst
+    }
+}
---
+++ src/test/kotlin/model/graph/algorithms/MinimumSpanningTreeTest.kt
@@ -0,0 +1,45 @@
+package model.graph.algorithms
+
+import model.graph.WeightedUndirectedGraph
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MinimumSpanningTreeTest {
+
+    @Test
+    fun testSingleEdgeGraph() {
+        val g = WeightedUndirectedGraph<Int, String>()
+        g.addEdge("A", "B", 1, 5.0)
+
+        val mst = MinimumSpanningTree(g).kruskalAlgo()
+        assertEquals(1, mst.size)
+        assertEquals(5.0, mst.first().weight)
+    }
+
+    @Test
+    fun testCycleGraph() {
+        val g = WeightedUndirectedGraph<Int, String>()
+        g.addEdge("A", "B", 1, 10.0)
+        g.addEdge("B", "C", 2, 11.0)
+        g.addEdge("A", "C", 3, 13.0)
+
+        val mst = MinimumSpanningTree(g).kruskalAlgo()
+        assertEquals(2, mst.size)
+        val totalWeight = mst.sumOf { it.weight }
+        assertEquals(21.0, totalWeight)
+    }
+
+    @Test
+    fun testGraphWithTwoComponents() {
+        val g = WeightedUndirectedGraph<Int, String>()
+        g.addEdge("A", "B", 1, 5.0)
+        g.addEdge("B", "C", 2, 6.0)
+
+        g.addEdge("X", "Y", 3, 7.0)
+
+        val mst = MinimumSpanningTree(g).kruskalAlgo()
+        assertEquals(3, mst.size)
+        val totalWeight = mst.sumOf { it.weight }
+        assertEquals(18.0, totalWeight)
+    }
+}
