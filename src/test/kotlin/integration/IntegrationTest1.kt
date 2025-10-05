f2051f519d23c11c4869d799967660ca2816cf41
Sterkachov <anymeshnik2.1@gmail.com>
2025-10-06 02:36:51 +0300

    test: добавил интеграционный тест на сохранение файла и алгоритм MST

---
+++ src/test/kotlin/integration/IntegrationTest1.kt
@@ -0,0 +1,50 @@
+package integration
+
+import androidx.compose.runtime.mutableStateOf
+import model.graph.WeightedUndirectedGraph
+import org.junit.Test
+import viewmodel.GraphColors
+import viewmodel.graph.GraphViewModel
+import java.io.File
+import kotlin.test.assertEquals
+
+class IntegrationTest1 {
+
+    @Test
+    fun testCreateLoadAndMstAlgorithm() {
+        val graph = WeightedUndirectedGraph<String, String>()
+
+        graph.addVertex("A")
+        graph.addVertex("B")
+        graph.addVertex("C")
+        graph.addVertex("D")
+
+        graph.addEdge("A", "B", "AB", 1.0)
+        graph.addEdge("A", "C", "AC", 2.0)
+        graph.addEdge("B", "C", "BC", 3.0)
+        graph.addEdge("C", "D", "CD", 4.0)
+
+        val viewModel = GraphViewModel(
+            graph,
+            mutableStateOf(true),
+            mutableStateOf(true)
+        )
+
+        val file = File.createTempFile("graph", ".json")
+        viewModel.saveGraph(file)
+
+        viewModel.loadGraph(file)
+
+        viewModel.highlightMst()
+
+        val highlighted = viewModel.edges.filter { edge ->
+            edge.color == GraphColors.Edge.highlighted
+        }
+
+        val highlightedLabels: Set<String> = highlighted.map { it.label }.toSet()
+
+        val expectedMst = setOf("AB", "CD", "AC")
+
+        assertEquals(expectedMst, highlightedLabels)
+    }
+}
